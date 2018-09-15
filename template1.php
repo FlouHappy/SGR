c0b5ecfd17484f29eef808a3749d57dd57795436
Franck Happy <hapf01@uqo.ca>
2018-09-15 11:58:06 -0400

    Modif de nom et creation de template

---
+++ template1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Systeme de Gestion des Resolutions</title>
+  <link rel="stylesheet" type="text/css" href="css/stylesheet.css" />
+  <link href="css/dycalendar.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <div id="page">
+    <div id="head" class="row">
+      <div id="head1"><br><br>
+        <a href="https://uqo.ca" title="Site officiel UQO.ca">UQO</a>
+      </div>
+      <div id="head2">
+        <h1 class="head2" ><a href="site.php">SGR</a></h1>
+        <h5 class="head21"><a href="site.php">Systeme de Gestion des Resolutions</a></h5>
+      </div>
+    </div>

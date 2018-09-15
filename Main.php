29cd8fa5242da4bd8bd0ef083197d1cb0dd8e2b9
Franck Happy <hapf01@uqo.ca>
2018-09-15 12:09:50 -0400

    0

---
+++ Main.php
@@ -0,0 +1,18 @@
+<?php include("template1.php") ?>
+
+
+
+<!--Iserer les formes -->
+
+
+
+<div id="container">
+  C'est ici qu'on va ajouter les differents formulaires
+</div>
+
+
+
+
+
+
+<?php include("template2.php") ?>
